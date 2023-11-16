a0b786e7d6d8f8d46fbffbe5b89a1a8118fc707a
haeril gunadi <haerilgunadi01@gmail.com>
2023-11-16 08:04:32 +0700

    TUGAS_pertemuan_2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return(
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App
